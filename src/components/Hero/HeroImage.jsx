6cec71ec29301447e965df8dbcc8521126104a5d
nasrinrahman19924 <nasrinrahman19924@gmail.com>
2026-09-08 21:48:56 +0600

    added new images

--- src/components/Hero/HeroImage.jsx
+++ src/components/Hero/HeroImage.jsx
@@ -1,4 +1,4 @@
-import profile from "../../assets/images/nas.jpg";
+import profile from "../../assets/images/shahin pc.jpg";
 import { motion } from "framer-motion";
 import hero from "../../data/hero";
 import heroIcons from "../../data/heroIcons";
